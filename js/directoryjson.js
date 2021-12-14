cdc9d7a2bdf2d75085fa1a3364466d5ecf742906
elmambou <esmambou@gmail.com>
2021-12-14 22:00:29 +0200

    updat

--- js/directoryjson.js
+++ js/directoryjson.js
@@ -12,16 +12,15 @@ fetch(requestURL)
         for (let i = 0; i < directories.length; i++) {
           let card = document.createElement('section');
           let name = document.createElement('h2');
-          let area= document.createElement('p');
           let image = document.createElement('img');
           let location = document.createElement('p');
-          let population = document.createElement('p');
-          let event = document.createElement('p');
-          let contact = document.createElement('h3');
+          let address = document.createElement('p');
+ 
+          let telephone = document.createElement('h3');
           let ord = document.createElement('h3');
-          let history = document.createElement('div');
-          let clo = document.createElement('h3');
-          let histories = document.createElement('h3');
+          let postaladdress = document.createElement('div');
+
+
           let ord2 = document.createElement('h3');
           let ord3 = document.createElement('h3');
           let linebreak = document.createElement('br');
@@ -29,72 +28,39 @@ fetch(requestURL)
           name.textContent = directories[i].name;
           area.textContent = "Area of the City: " + directories[i].area;
           location.textContent = "City Location: " + directories[i].location;
-          population.textContent = "Population: " + directories[i].population;
-          event.textContent = "Upcoming Events: " + directories[i].event;
+          address.textContent = "Population: " + directories[i].address;
+
           image.setAttribute('src', directories[i].imageurl);
           image.setAttribute('alt', "Beautiful Image of " + directories[i].name);
-          contact.textContent = "Business Information";
-          histories.textContent = "Important Historical Facts";
+          telephone.textContent = "Business Information";
+    
           
           
           
-          for (let a = 0; a < directories[i].history.length; a++) {
+          for (let a = 0; a < directories[i].postaladdress.length; a++) {
             let hist = document.createElement('p');
-            hist.textContent = directories[i].history[a];
-            history.appendChild(hist);
+            hist.textContent = directories[i].postaladdress[a];
+            postaladdress.appendChild(hist);
          }
 
           // Adding information under section tag 
           card.appendChild(name); 
           card.appendChild(image);
-          card.appendChild(contact);
+          card.appendChild(telephone);
           card.appendChild(area);
           card.appendChild(location); 
           card.appendChild(ord);
           card.appendChild(ord2);
           card.appendChild(ord3);
-          card.appendChild(population);
-          card.appendChild(clo);
-          card.appendChild(event);
-          card.appendChild(histories);
-          card.appendChild(history);
+          card.appendChild(address);
+     
+          card.appendChild(postaladdress);
            
           // Adding to information to card class
           document.querySelector('div.cards').appendChild(card);
 
          let hr = document.createElement('hr');
 
-         const cityWeatherURL = "https://api.openweathermap.org/data/2.5/weather?id=" + directories[i].id + "&units=imperial&APPID=200cf102e64ee34534ad4bd4429e42bc";
-
-         fetch(cityWeatherURL)
-         .then((response) => response.json())
-         .then((jsObject) => {
-
-         // Set variable to JSON data values
-      let high = Math.floor(jsObject.main.temp_max);
-      let humidity = jsObject.main.humidity;
-      let windSpeed = Math.floor(jsObject.wind.speed);
-      let currentTemp = Math.floor(jsObject.main.temp);
-      let wsum = document.createElement('div');
-      let highTemp = document.createElement('p');
-      let hum = document.createElement('p');
-      let speedOfWind = document.createElement('p');
-      let tempOfCurrentTimes = document.createElement('p');
-      let wsumTitle = document.createElement('h3');
-
-      highTemp.innerHTML = "High: " + high + '&degF';
-      hum.textContent = "Humidity: " + humidity + "%";
-      tempOfCurrentTimes.innerHTML = "Current: " + currentTemp + '&degF';
-      speedOfWind.textContent = "Wind Speed: " + windSpeed + "mph";
-      wsumTitle.textContent = "Weather Summary";
-
-      wsum.appendChild(highTemp);
-      wsum.appendChild(hum);
-      wsum.appendChild(tempOfCurrentTimes);
-      wsum.appendChild(speedOfWind);
-      card.appendChild(wsumTitle);
-      card.appendChild(wsum);
-      card.appendChild(linebreak);
-      card.appendChild(hr);
-
-      })}});
+  
+     
+      }});
