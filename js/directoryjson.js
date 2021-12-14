41813d6a10b7db6dde3fe3ed43f2e0e2fe593d67
elmambou <esmambou@gmail.com>
2021-12-14 22:35:22 +0200

    kkk

--- js/directoryjson.js
+++ js/directoryjson.js
@@ -11,32 +11,59 @@ fetch(requestURL)
         
         for (let i = 0; i < directories.length; i++) {
           let card = document.createElement('section');
-          let name = document.createElement('h2');
+          let p1 = document.createElement('h2');
           let image = document.createElement('img');
-          let location = document.createElement('p');
-          let address = document.createElement('p');
+          let div = document.createElement("div");
+          let p2 = document.createElement('p');
+          let p3 = document.createElement('p');
  
-          let telephone = document.createElement('h3');
-          let ord = document.createElement('h3');
+          let p4 = document.createElement('h3');
+
           let postaladdress = document.createElement('div');
 
-
-          let ord2 = document.createElement('h3');
-          let ord3 = document.createElement('h3');
           let linebreak = document.createElement('br');
 
-          name.textContent = directories[i].name;
-          area.textContent = "Area of the City: " + directories[i].area;
-          location.textContent = "City Location: " + directories[i].location;
-          address.textContent = "Population: " + directories[i].address;
+          //name.textContent = directories[i].name;
+          //area.textContent = "Area of the City: " + directories[i].area;
+          //location.textContent = "City Location: " + directories[i].location;
+          //address.textContent = "Population: " + directories[i].address;
 
           image.setAttribute('src', directories[i].imageurl);
           image.setAttribute('alt', "Beautiful Image of " + directories[i].name);
-          telephone.textContent = "Business Information";
+          //telephone.textContent = "Business Information";
     
-          
-          
-          
+          div.setAttribute("class", "urlLink");
+          card.setAttribute("class", "businessBox");
+
+          p1.innerHTML = `${directories.name}`;
+          p2.innerHTML = `${directories.address},${directories.postaladdress}`;
+          p3.innerHTML = `${directories.location}`;
+          p4.innerHTML = `${directories.telephone}`;
+
+          div.append(p1);
+          div.append(p2);
+          div.append(p3);
+          div.append(p4);
+
+          card.append(image);
+          card.append(div);
+
+          document.querySelector("div.businessDirectory").append(card);
+      }});  
+
+const list = document.querySelector(".list");
+const grid = document.querySelector(".grid");
+const businessDirectory = document.querySelector(".businessDirectory");
+
+list.addEventListener("click", () => {
+  businessDirectory.classList.add("list");
+});
+
+grid.addEventListener("click", () => {
+  businessDirectory.classList.remove("list");
+});
+
+         /* 
           for (let a = 0; a < directories[i].postaladdress.length; a++) {
             let hist = document.createElement('p');
             hist.textContent = directories[i].postaladdress[a];
@@ -46,12 +73,9 @@ fetch(requestURL)
           // Adding information under section tag 
           card.appendChild(name); 
           card.appendChild(image);
-          card.appendChild(telephone);
-          card.appendChild(area);
+          card.appendChild(telephone);      
           card.appendChild(location); 
-          card.appendChild(ord);
-          card.appendChild(ord2);
-          card.appendChild(ord3);
+
           card.appendChild(address);
      
           card.appendChild(postaladdress);
@@ -63,4 +87,4 @@ fetch(requestURL)
 
   
      
-      }});
+      }});*/
