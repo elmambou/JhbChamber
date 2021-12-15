335dbda4c3575257a852daf849950b3cfe9f0b99
elmambou <esmambou@gmail.com>
2021-12-15 21:22:48 +0200

    jhy

--- js/cityThreeDaysForecast.js
+++ js/cityThreeDaysForecast.js
@@ -1,60 +1,44 @@
-// API.OPENWEATHERMAP.ORG Forecast JSON URL
-const apiURLForecast = "https://api.openweathermap.org/data/2.5/forecast?zip=83287&units=imperial&appid=200cf102e64ee34534ad4bd4429e42bc";
+const apiKey = "d26fd24f1d08cf466081800bd5bc4381";
+// Lat & Lon for Cardston, Alberta, Canada
+const lat = "26.0963";
+const lon = "27.8077";
 
-// Fetch API Forecast URL
-fetch(apiURLForecast)
+const apiURL = `//api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
-    .then((response) => response.json())
-    .then((jsObject) => {
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    // Checks to see if there are any alerts property
+    if (!weatherInfo.hasOwnProperty("alerts")) {
+      document.querySelector("#warningWrapper").style.display = "none";
+    } else {
+      let warning = weatherInfo.alerts[0].description;
+      document.getElementById("weatherWarning").innerHTML = warning;
+    }
 
-        // Iterate through the JSON for all 5 Forecast days 
-        for (let i = 0; i < jsObject.list.length; i++) {
+    // Calculates temperature in Fahrenheit
+    // const currentTemp = `${Math.round(weatherInfo.current.temp)}&deg;F`;
 
-            // IF statement that checks JSON values for 18:00:00
-            if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+    // Calculates temperature in Celcius
+    const currentTemp = `${Math.round(
+      (5 / 9) * (weatherInfo.current.temp - 32)
+    )}&deg;C`;
 
-                // Setting card, name of day, image, and paragraphs
-                let card = document.createElement('section');
-                let day = document.createElement('h3');
-                let image = document.createElement('img');
-                let p1 = document.createElement('p');
-                let p2 = document.createElement('p');
-                let p3 = document.createElement('p');
+    const descr = weatherInfo.current.weather[0].description.replace(
+      /(^\w{1})|(\s+\w{1})/g,
+      (letter) => letter.toUpperCase()
+    );
+    const humidity = `${Math.round(weatherInfo.current.humidity)}%`;
 
-                // Retrieving the icon 'code' and URL for current weather image SRC
-                let src = 'https://openweathermap.org/img/wn/' + jsObject.list[i].weather[0].icon + '@2x.png';
+    document.getElementById("current-temp").innerHTML = currentTemp;
+    document.getElementById("description").innerHTML = descr;
+    document.getElementById("humidity").innerHTML = humidity;
+  });
 
-                // Retrieving weather description for image ALT
-                let alt = jsObject.list[i].weather[0].description;
-
-                // Set day name array and current date
-                let dayArr = ["SUN","MON","TUES","WED","THURS","FRI","SAT"];
-                let datum = new Date(jsObject.list[i].dt_txt);
-                
-                /*
-                 * Setting:
-                 *          Name of Day
-                 *          Temperature (Current)
-                 *          Wind Speed
-                 *          Humidity Level
-                 *          Image SRC + ALT
-                 */
-                day.textContent = dayArr[datum.getDay()];
-                p1.innerHTML = "Temperature: " +  jsObject.list[i].main.temp + '&#8457;';
-                p2.innerHTML = "Wind Speed: " + jsObject.list[i].wind.speed + " mph";
-                p3.innerHTML = 'Humidity: ' +  jsObject.list[i].main.humidity + "%";
-                image.setAttribute('src', src);
-                image.setAttribute('alt', alt);
-                
-                // Appending the new variables to the section card
-                card.appendChild(day);
-                card.appendChild(image);
-                card.appendChild(p1);
-                card.appendChild(p2);
-                card.appendChild(p3);
-
-                // Appending card to 'div.forecast' in HTML
-                document.querySelector('div.forecast').appendChild(card);
-            }
-        }
-    });
+const targetDiv = document.getElementById("warningWrapper");
+const btn = document.getElementById("close");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  }
+};
