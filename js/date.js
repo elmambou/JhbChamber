b0e0cd94617f2276ff5195074d475984bcf029fb
elmambou <77119578+elmambou@users.noreply.github.com>
2021-12-16 19:20:41 +0200

    Update date.js

--- js/date.js
+++ js/date.js
@@ -1,6 +1,5 @@
-
-
-var dateOptions = {
+// Last Modified Date
+const dateOptions = {
    weekday: "long",
    day: "numeric",
    month: "long",
@@ -8,3 +7,26 @@ var dateOptions = {
 }
 document.getElementById('fullDate').innerHTML = new Date().toLocaleString("en-ZA", dateOptions);
 
+
+
+closeBannerBtn.addEventListener("click", function () {
+  banner.classList.add("hide");
+});
+
+
+// Store alerts
+let alerts;
+
+function handleAlerts() {
+  // Check for alerts
+  if (alerts) {
+    banner.classList.remove("hide");
+    const p = document.createElement("p");
+    p.textContent = alerts[0].description;
+    banner.appendChild(p);
+  } else {
+    banner.classList.add("hide");
+  }
+}
+
+handleAlerts();
